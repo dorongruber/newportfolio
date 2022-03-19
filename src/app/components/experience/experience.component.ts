864246afeb895609291db53ef3e71712b5a7e6c6
doron <doron.gruber@gmail.com>
2022-03-19 09:24:56 +0200

    update main header style

--- src/app/components/experience/experience.component.ts
+++ src/app/components/experience/experience.component.ts
@@ -3,7 +3,8 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-experience',
   templateUrl: './experience.component.html',
-  styleUrls: ['./experience.component.css']
+  styleUrls: ['./experience.component.css'
+    ,'../../styles/mainheader.component.css']
 })
 export class ExperienceComponent implements OnInit {
 
